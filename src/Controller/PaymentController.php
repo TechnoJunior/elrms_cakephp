928a0c3a792fb0b24d6500835a274a89ec25bad6
TechnoBoy <dloadjunior@gmail.com>
2020-02-20 13:14:15 +0530

    Loaded 2 Models

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -7,11 +7,12 @@
     {
         public function initialize(): void {
             parent::initialize();
-            $this->loadModel('Payments');
+            $this->loadModel(['Payments','Divisions']);
             $this->viewBuilder()->setLayout('niceadmin');
         }
         
         public function add() {
+            $division_list= $this->Payments->find('all',['order'=>'Payment_id']);
             $this->set('title','Add Payment');
         }
     }
